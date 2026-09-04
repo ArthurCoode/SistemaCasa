0c71e1bc8fe754571ab3e17f959476489a687631
Arthur Marques <arthurvilasboas2020@gmail.com>
2026-09-03 21:52:11 -0300

    chore: adiciona rotas para gerenciamento de alimentos

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlimentosController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('criarAlimento',[AlimentosController::class, 'criarAlimento'])->name('criarAlimento ');
+Route::get('listarAlimentos',[AlimentosController::class, 'listarAlimentos'])->name('listarAlimento');
+Route::post('atualizarAlimento',[AlimentosController::class, 'atualizarAlimento'])->name('atualizarAlimento');
+Route::delete('deletarAlimento',[AlimentosController::class, 'deletarAlimento'])->name('deletarAlimento');
